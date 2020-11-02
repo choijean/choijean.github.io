f123efce3a047ceb7b3cc144b00f646c2aa7ccef
Jean Choi <choijean@pdx.edu>
2020-11-01 20:25:21 -0800

    new navbar

--- js/navbar.js
+++ js/navbar.js
@@ -6,16 +6,26 @@ $(document).ready(function () {
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
     $(this).toggleClass("active");
   });
+  // // Smoothscroll script
+  // $(".smooth-scroll").click(function (e) {
+  //   e.preventDefault();
+  //   var dis = $(this),
+  //     target = dis.attr("href"),
+  //     offset = parseInt($(target).offset().top),
+  //     header = $(".navbar");
+  //   dis.addClass("active").parent().siblings().find(".smooth-scroll").removeClass("active");
+  //   $('html,body').stop().animate({ scrollTop: offset }, 200);
+  // });
   // Closes responsive menu when a scroll trigger link is clicked
   $('#navbar-wrapper').click(function () {
     $("#navbar-wrapper").removeClass("active");
     $(".menu-toggle").removeClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
   });
-  if ($(window).width() > 991) {
-    $(window).on("load", function (e) {
-      $("body").addClass("active");
-    })
-  }
+  // if ($(window).width() > 991) {
+  //   $(window).on("load", function (e) {
+  //     $("body").addClass("active");
+  //   })
+  // }
 });
 
